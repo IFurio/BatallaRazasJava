92b580e2056f9afa925c2637fc0b2dc5adf7683b
VDani3 <villacarmona.dani@gmail.com>
2023-05-16 19:08:42 +0200

    bug patch

--- M4-LleguatgedeMarques/dataToXml.py
+++ M4-LleguatgedeMarques/dataToXml.py
@@ -7,8 +7,8 @@ def collectData():
     config = {
             'host':'127.0.0.1', 
             'port':'3306',
-            'user':'user', 
-            'password':'passwd', 
+            'user':'admin', 
+            'password':'1111', 
             'database':'batalla_races'}
     try:
         conexion = mysql.connector.connect(**config)
@@ -29,10 +29,10 @@ def createXML(data):
     tab2 = tab + tab + tab
     xml = '<?xml version="1.0" encoding="UTF-8"?>' + sep + '<!DOCTYPE battle SYSTEM "battle.dtd">' + sep + '<battle>' + sep 
     for row in data:
-        xml += '<battle_info>'
-        xml += '<battle_id>'+ str(row[0]) + '</battle_id>' + sep + tab2 + '<player_id>'+ str(row[1]) + '</player_id>' + sep + tab2 + '<warrior_id>' + str(row[2]) + '</warrior_id>' + sep + tab2 + '<warrior_weapon_id>' + str(row[3]) + '</warrior_weapon_id>' + sep + tab2 + '<opponent_id>' + str(row[4]) + '</opponent_id>' + sep + tab2 + '<opponent_weapon_id>' + str(row[5]) + '</opponent_weapon_id>' + sep + tab2 + '<injuries_caused>' + str(row[6]) + '</injuries_caused>' + sep + tab2 + '<injuries_suffered>'+ str(row[7]) + '</injuries_suffered>' + sep + tab2 + '<battle_points>'+ str(row[8]) + '</battle_points>' + sep + '</battle>' + sep
-        xml += '</battle_info>'
-    file = open('./xml/battle.xml', "w")
+        xml += '<battle_info>'+sep
+        xml += '<battle_id>'+ str(row[0]) + '</battle_id>' + sep + tab2 + '<player_id>'+ str(row[1]) + '</player_id>' + sep + tab2 + '<warrior_id>' + str(row[2]) + '</warrior_id>' + sep + tab2 + '<warrior_weapon_id>' + str(row[3]) + '</warrior_weapon_id>' + sep + tab2 + '<opponent_id>' + str(row[4]) + '</opponent_id>' + sep + tab2 + '<opponent_weapon_id>' + str(row[5]) + '</opponent_weapon_id>' + sep + tab2 + '<injuries_caused>' + str(row[6]) + '</injuries_caused>' + sep + tab2 + '<injuries_suffered>'+ str(row[7]) + '</injuries_suffered>' + sep + tab2 + '<battle_points>'+ str(row[8]) + '</battle_points>' + sep + '</battle_info>' + sep
+    xml += '</battle>'
+    file = open("./xml/battle.xml", "w")
     file.write(xml)
     file.close()
 
